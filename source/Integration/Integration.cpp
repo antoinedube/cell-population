ffc5e2a2660f7a5dd06f9f42595fa8792be53c3f
Antoine Dubé <antoine.dube@gmail.com>
2016-09-16 16:16:15 -0400

    Progress on integration

--- source/Integration/Integration.cpp
+++ source/Integration/Integration.cpp
@@ -1,9 +1,11 @@
 #include "Integration/Integration.hpp"
 #include "Models/Parameters.hpp"
+#include "Models/System.hpp"
 #include "Random/MersenneTwister.hpp"
 
-Integration::Integration(Parameters *parameters) {
+Integration::Integration(Parameters *parameters, System *system) {
   this->parameters = parameters;
+  this->system = system;
 }
 
 Integration::~Integration() {
@@ -12,7 +14,8 @@ Integration::~Integration() {
 void Integration::run() {
   for (int cycle=0; cycle<this->parameters->n_cycles; cycle++) {
     for (int step=0; step<this->parameters->n_steps; step++) {
-      // system.pick_particle()
+      this->system->pick_particle();
+
       // Take random step
 
       // Check if site is available
--- source/Integration/Integration.hpp
+++ source/Integration/Integration.hpp
@@ -2,15 +2,17 @@
 #define INTEGRATION_HPP
 
 #include "Models/Parameters.hpp"
+#include "Models/System.hpp"
 
 class Integration {
 public:
-  Integration(Parameters *parameters);
+  Integration(Parameters *parameters, System *system);
   virtual ~Integration();
 
   virtual void run();
 private:
   Parameters *parameters;
+  System *system;
 };
 
 #endif
